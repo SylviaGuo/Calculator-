8eb8ea0ac7e74038bbf4fc0625d99a45294175aa
Sylvia <Sylviaguo31@gmail.com>
2019-09-11 13:19:59 +1200

    build Main storyboard - calculator interface

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by wendy on 2019/9/11.
+//  Copyright © 2019 Chunli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
