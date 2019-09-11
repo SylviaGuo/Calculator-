e6222e8bc4602d95ca8736255b3fc13fd04ad758
Sylvia <Sylviaguo31@gmail.com>
2019-09-11 16:45:58 +1200

    1. Create outlet and actions for related buttons; 2. Implementation of calculating +/- and %.

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    var firstResult: Double = 0
+    var secondInput: Double = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    var currentNum:Double = 0
+    var currentOperator:String = ""
+    @IBAction func DigitsPressed(_ sender: UIButton) {
+        resultLabel.text! += sender.titleLabel!.text!
+    }
+    
+    @IBAction func ClearScreenPressed(_ sender: UIButton) {
+        resultLabel.text = "0"
+    }
+    
+    @IBAction func OperatorPressed(_ sender: UIButton) {
+        currentNum = Double(resultLabel.text!)!
+        currentOperator = sender.titleLabel!.text!
+        if currentOperator == "+/-"{
+            currentNum *= -1
+            resultLabel.text = "\(currentNum)"
+        }else if currentOperator == "%"{
+            currentNum /= 100
+            resultLabel.text = "\(currentNum)"
+        }else{
+            
+        }
+    }
+    
+    @IBAction func CalculatePressed(_ sender: UIButton) {
+        if currentOperator == "+"{
+            
+        }
+    }
+    
 }
 
